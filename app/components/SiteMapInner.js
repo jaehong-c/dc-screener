b02be64b0828ed11caa5fff88e64a823358aeeb5
Jaehong Chung <jc6412@columbia.edu>
2026-09-24 14:26:13 -0400

    Update SiteMapInner component to include dynamic CARTO API key in tile layer URL

--- app/components/SiteMapInner.js
+++ app/components/SiteMapInner.js
@@ -68,7 +68,7 @@ export default function SiteMapInner({ result }) {
         >
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OSM</a> &copy; <a href="https://carto.com/attributions">CARTO</a>'
-            url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+            url={`https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png?key=${process.env.NEXT_PUBLIC_CARTO_KEY}`}
           />
 
           <FitBounds points={pointsForBounds} />
